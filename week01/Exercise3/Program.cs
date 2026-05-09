44e6b5f202cb3e162cc2de8f385cace23eeea5a5
kabandaronald70-create <kabandaronal70@gmail.com>
2026-05-09 18:37:46 +0300

    changes in exercise 3 program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int gues = -1;
+        while (gues != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            gues = int.Parse(Console.ReadLine());
+
+            if (magicNumber > gues)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (gues > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
